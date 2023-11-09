40e848b73e1009dfabf2acb41535f51733bb353b
matan <matan@iammaran.com>
2023-11-09 16:36:21 +0200

    sharing is caring

--- resources/js/controllers/manage-workflow_controller.js
+++ resources/js/controllers/manage-workflow_controller.js
@@ -1,10 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "wsLink" ]
+  static targets = [ "wsLink" , "genLink", "embedCode"]
   copyLink(event) {
     event.preventDefault()
     navigator.clipboard.writeText(this.wsLinkTarget.value)
+    event.target.innerText = "Copied!"
+    event.target.classList.add("text-red-300")
+    event.target.classList.remove("text-red-500")
+    setTimeout(() => {
+      event.target.innerText = "Copy"
+      event.target.classList.add("text-red-500")
+      event.target.classList.remove("text-red-300")
+    }, 1500)
+  }
+
+  copyGenerateLink(event) {
+    event.preventDefault()
+    navigator.clipboard.writeText(this.genLinkTarget.value)
+    event.target.innerText = "Copied!"
+    event.target.classList.add("text-red-300")
+    event.target.classList.remove("text-red-500")
+    setTimeout(() => {
+      event.target.innerText = "Copy"
+      event.target.classList.add("text-red-500")
+      event.target.classList.remove("text-red-300")
+    }, 1500)
+  }
+
+  copyEmbedCode(event) {
+    event.preventDefault()
+    navigator.clipboard.writeText(this.embedCodeTarget.value)
     event.target.innerText = "Copied!"
     event.target.classList.add("text-red-300")
     event.target.classList.remove("text-red-500")
